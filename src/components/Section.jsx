2207d4d4c9d3b61ccb4c390f04dbfb915c67ab52
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2022-02-24 00:21:26 +0300

    Project for tesla clone created. The main page, header and hamburger menu created.

---
+++ src/components/Section.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+import Fade from 'react-reveal/Fade'
+
+function Section({ title, description, backgroundImg, leftButtonText, rightButtonText }) {
+    return (
+        <Wrap backgroundImage={backgroundImg}>
+            {/* React Reveal is an animation library for React */}
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
+            <Buttons>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftButtonText}
+                        </LeftButton>
+                        {/* Right button will only exists if there is a right button text*/}
+                        {rightButtonText && <RightButton>
+                            {rightButtonText}
+                        </RightButton>}
+                    </ButtonGroup>
+                </Fade>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
+
+        </Wrap >
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: ${props => `url("/images/${props.backgroundImage}")`};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+    align-items: center;
+`;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+
+    @media (max-width: 768px){
+        flex-direction: column;
+    }
+`;
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    margin: 8px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+`;
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;   
+    animation: animateDown infinite 1.5s;  //Created in index.css
+`;
+
+const Buttons = styled.div`
+
+`;
